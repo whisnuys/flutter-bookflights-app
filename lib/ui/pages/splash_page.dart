c5ee231a8e19777439b1b35ed457a031e4afc8fc
whisnuys <whisnusaputra30@gmail.com>
2022-06-04 06:31:36 +0700

    splash screen done

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: EdgeInsets.only(bottom: 50),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icon_plane.png'),
+                ),
+              ),
+            ),
+            Text(
+              'AIRPLANE',
+              style: whiteTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: semiBold,
+                letterSpacing: 10,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
