79cafcf1efb21021409bade2d0912dbbc4fb8164
whisnuys <whisnusaputra30@gmail.com>
2022-06-06 05:58:38 +0700

    added some widget signup page

--- lib/ui/pages/signup_page.dart
+++ lib/ui/pages/signup_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
 import '../../shared/theme.dart';
@@ -22,37 +22,149 @@ class SignUpPage extends StatelessWidget {
     }
 
     Widget InputSection() {
-      Widget inputForm() {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Full Name',
-              style: blackTextStyle,
-            ),
-            SizedBox(
-              height: 6,
-            ),
-            TextFormField(
-              cursorColor: kBlackColor,
-              decoration: InputDecoration(
-                hintText: "Your full name",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(
-                    defaultRadius,
+      Widget nameInput() {
+        return Container(
+          margin: EdgeInsets.only(
+            bottom: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full Name',
+                style: blackTextStyle,
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: "Your full name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    ),
                   ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(
-                    defaultRadius,
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget emailInput() {
+        return Container(
+          margin: EdgeInsets.only(
+            bottom: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Email Address',
+                style: blackTextStyle,
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: "Your email address",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
                   ),
-                  borderSide: BorderSide(
-                    color: kPrimaryColor,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    ),
                   ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget passwordInput() {
+        return Container(
+          margin: EdgeInsets.only(
+            bottom: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Password',
+                style: blackTextStyle,
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                obscureText: true,
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: "Your password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget submitButton() {
+        return Container(
+          width: double.infinity,
+          height: 55,
+          margin: EdgeInsets.only(
+            top: 10,
+          ),
+          child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  defaultRadius,
                 ),
               ),
-            )
-          ],
+            ),
+            child: Text(
+              'Get Started',
+              style: whiteTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: medium,
+              ),
+            ),
+          ),
         );
       }
 
@@ -73,8 +185,29 @@ class SignUpPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            inputForm(),
+            nameInput(),
+            emailInput(),
+            passwordInput(),
+            submitButton(),
           ],
+        ),
+      );
+    }
+
+    Widget tacButton() {
+      return Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(
+          top: 50,
+          bottom: 73,
+        ),
+        child: Text(
+          'Terms and Conditions',
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: light,
+            decoration: TextDecoration.underline,
+          ),
         ),
       );
     }
@@ -89,6 +222,7 @@ class SignUpPage extends StatelessWidget {
           children: [
             title(),
             InputSection(),
+            tacButton(),
           ],
         ),
       ),
