e73f2814c34a5213dfcdaead5906cfd75e1d44e1
whisnuys <whisnusaputra30@gmail.com>
2022-07-09 14:12:21 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,16 @@ import 'package:bwa_airplane/ui/pages/get_started_page.dart';
 import 'package:bwa_airplane/ui/pages/main_page.dart';
 import 'package:bwa_airplane/ui/pages/signup_page.dart';
 import 'package:bwa_airplane/ui/pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
